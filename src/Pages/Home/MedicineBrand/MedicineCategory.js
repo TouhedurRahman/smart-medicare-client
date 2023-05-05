0754fe9ebde769336579a68f428c284ad837fa19
TouhedurRahman <touhedur.cse@gmail.com>
2023-05-05 20:25:07 +0600

    medicine category card design updated

--- src/Pages/Home/MedicineBrand/MedicineCategory.js
+++ src/Pages/Home/MedicineBrand/MedicineCategory.js
@@ -2,23 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const MedicineCategory = ({ brand }) => {
-    
+
     return (
         <div className='container mx-auto'>
             <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 mx-auto px-4 py-10   justify-center align-center'>
-                {brand.map((item, index) => <Link to={`/medicinebrand/brand/${item._id}`} key={index}>
-                    <div className=" rounded overflow-hidden shadow-lg">
-                        <img className="w-full border" src={item.imgRef} alt={brand.name} />
-                         
-                       
-                        <div className="px-6 py-4 text-center">
-                            {/* <div className="font-bold text-xl mb-2">{item.name}</div> */}
-                            <h1 className='my-3'>{item.brandTitle}</h1>
-                            <h1 className='my-3 font-bold text-xl mb-2'>{item.name}</h1>
-                            <p className="text-gray-700 text-base">{item.description}</p>
+                {
+                    brand.map((item, index) => <Link
+                        to={`/medicinebrand/brand/${item._id}`}
+                        key={index}
+                    >
+                        <div className=" rounded overflow-hidden shadow-lg">
+                            <img className="w-full border" src={item.imgRef} alt="Loading" style={{ height: "250px" }} />
+                            <div className="px-6 py-4 text-center">
+                                <h1 className='my-3'>{item.brandTitle}</h1>
+                                <h1 className='my-3 font-bold text-xl mb-2'>{item.name}</h1>
+                                <p className="text-gray-700 text-base">{item.description}</p>
+                            </div>
                         </div>
-                    </div>
-                </Link>)}
+                    </Link>)
+                }
             </div>
         </div>
     );
