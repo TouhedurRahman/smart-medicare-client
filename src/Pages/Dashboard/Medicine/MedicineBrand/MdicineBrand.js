fea2a9a2104feed4e1edc838255d6544e8a2f8a1
TouhedurRahman <touhedur.cse@gmail.com>
2023-05-05 18:53:14 +0600

    all medicine brands page updated on dashboard

--- src/Pages/Dashboard/Medicine/MedicineBrand/MdicineBrand.js
+++ src/Pages/Dashboard/Medicine/MedicineBrand/MdicineBrand.js
@@ -3,70 +3,67 @@ import React, { useEffect, useState } from 'react';
 import { AiFillDelete } from 'react-icons/ai';
 
 const MdicineBrand = () => {
-    const [medicineBrand, setmedicineBrand] = useState([])
+    const [medicineBrand, setmedicineBrand] = useState([]);
+
     useEffect(() => {
         axios.get(`http://localhost:5000/api/v1/medicinebrand`)
             .then(response => {
-
                 setmedicineBrand(response.data.result)
             })
-            .catch(error => {
-                // setError(error.message)
-            })
-    }, [])
-    const deleteBrand = (e) => {
-        // e.preventDefault();
+            .catch(error => { })
+    }, []);
 
+    const deleteBrand = (e) => {
         axios.delete(`http://localhost:5000/api/v1/medicinebrand/${e}`)
             .then(response => {
                 const filter = medicineBrand.filter(item => item._id !== e);
-                setmedicineBrand(filter)
-                // setAppointment(res.data.result)
+                setmedicineBrand(filter);
             })
-            .catch(error => {
-                // setError(error.message)
-            })
-
+            .catch(error => { })
     }
+
     return (
         <div className='container mx-auto'>
+            <div>
+                <h3 className='text-3xl text-center text-[#0E7490] font-bold m-5'>
+                    <i>
+                        All Medicine Brands_
+                    </i>
+                </h3>
+            </div>
             <div className="overflow-x-auto w-full">
                 <table className="table w-full">
                     <thead>
                         <tr>
-                            <th>Image</th>
-                            <th>Brand Name</th>
+                            <th className='text-center'>Image</th>
+                            <th className='text-center'>Brand Name</th>
+                            <th className='text-center'>Delete</th>
 
-                            <th>Action</th>
-                           
                         </tr>
                     </thead>
                     <tbody>
                         {
                             medicineBrand.map((brand) => <tr key={brand._id}>
                                 <td>
-                                    <div className="flex items-center space-x-3">
+                                    <div className="flex justify-center items-center space-x-3">
                                         <div className="avatar">
                                             <div className="mask mask-squircle w-12 h-12">
                                                 <img src={brand.imgRef} alt="Avatar Tailwind CSS Component" />
                                             </div>
                                         </div>
-                                       
                                     </div>
                                 </td>
- 
- 
                                 <th>
-                                    <div>
+                                    <div className='flex justify-center items-center'>
                                         <div className="font-bold">{brand.name}</div>
                                     </div>
-
                                 </th>
                                 <th>
-                                    <button title="delete" className="btn btn-ghost" onClick={() => deleteBrand(brand._id)}>
-                                        < AiFillDelete size={22} />
-                                    </button>
-
+                                    <div className='flex justify-center items-center'>
+                                        <button title="delete" className="btn btn-ghost" onClick={() => deleteBrand(brand._id)}>
+                                            < AiFillDelete size={22} />
+                                        </button>
+                                    </div>
                                 </th>
                             </tr>)
                         }
