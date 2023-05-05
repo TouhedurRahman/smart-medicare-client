5652d16963e0d8af618dfd5c800f33dcd769082f
TouhedurRahman <touhedur.cse@gmail.com>
2023-05-05 19:17:12 +0600

    add medicine page updated on dashboard

--- src/Pages/Dashboard/Medicine/Medicine/AddMedicine.js
+++ src/Pages/Dashboard/Medicine/Medicine/AddMedicine.js
@@ -3,6 +3,8 @@ import React, { useRef, useState } from 'react';
 import { toast } from 'react-hot-toast';
 
 const AddMedicine = () => {
+    const [error, setError] = useState("");
+
     const nameRef = useRef();
     const descriptionRef = useRef();
     const categoryRef = useRef();
@@ -10,7 +12,7 @@ const AddMedicine = () => {
     const priceRef = useRef();
     const brandNameRef = useRef();
     const brandIdRef = useRef();
-    const [error, setError] = useState("")
+
     const handleOnSubmit = (e) => {
         axios.post('http://localhost:5000/api/v1/medicine', {
             name: nameRef.current.value,
@@ -24,11 +26,10 @@ const AddMedicine = () => {
             }
         })
             .then(res => {
-                toast.success(<h2>congrats you can successfully added medicine  </h2>)
+                toast.success("Medicine added successfully");
             })
             .catch(error => {
-                // setError(error.response.data.error)
-                toast.error(<h1>  {error?.response?.data?.error}</h1>);
+                toast.error(`${error?.response?.data?.error}`);
             })
         nameRef.current.value = ""
         categoryRef.current.value = ""
@@ -39,42 +40,49 @@ const AddMedicine = () => {
         brandIdRef.current.value = ""
         e.preventDefault()
     }
+
     return (
         <div className='container mx-auto'>
             {error && <h1>{error}</h1>}
-            <h1 className='text-center text-3xl text-indigo-800 my-10 '>Add Medicine  </h1>
+            <div>
+                <h3 className='text-3xl text-center text-[#0E7490] font-bold m-5'>
+                    <i>
+                        Add Medicine_
+                    </i>
+                </h3>
+            </div>
             <div className="form-control px-4">
-                <form className='bg-zinc-200 p-4' onSubmit={handleOnSubmit}>
+                <form className='bg-zinc-200 p-4 rounded-xl' onSubmit={handleOnSubmit}>
                     <label className="input-group">
-                        <span className="w-40">Medicine Name</span>
+                        <span className="w-40 font-bold">Medicine Name</span>
                         <input required type="text" ref={nameRef} placeholder="Enter medicine name" className="border-4 border-indigo-500/100 ..." />
                     </label>
                     <label className="input-group">
-                        <span className="w-40">Medicine Image Url</span>
-                        <input required type="text" ref={imgRef} placeholder="Enter img link" className="border-4 border-indigo-500/100 ..." />
+                        <span className="w-40 font-bold">Medicine Image</span>
+                        <input required type="text" ref={imgRef} placeholder="Enter Medicine Image URL" className="border-4 border-indigo-500/100 ..." />
                     </label>
                     <label className="input-group">
-                        <span className="w-40">medicine category</span>
-                        <input required type="text" ref={categoryRef} placeholder="Enter category" className="border-4 border-indigo-500/100 ..." />
+                        <span className="w-40 font-bold">Description</span>
+                        <input required type="text" ref={categoryRef} placeholder="Enter Medicine Description" className="border-4 border-indigo-500/100 ..." />
                     </label>
                     <label className="input-group">
-                        <span className="w-40">brand Description</span>
-                        <input required type="text" ref={descriptionRef} placeholder="Enter qualification" className="border-4 border-indigo-500/100 ..." />
+                        <span className="w-40 font-bold">Medicine Brand</span>
+                        <input required type="text" ref={descriptionRef} placeholder="Medicine Brand" className="border-4 border-indigo-500/100 ..." />
                     </label>
                     <label className="input-group">
-                        <span className="w-40">Medicine Price</span>
-                        <input required type="number" ref={priceRef} placeholder="Enter price" className="border-4 border-indigo-500/100 ..." />
+                        <span className="w-40 font-bold">Medicine Price</span>
+                        <input required type="number" ref={priceRef} placeholder="Enter Medicine price" className="border-4 border-indigo-500/100 ..." />
                     </label>
                     <label className="input-group">
-                        <span className="w-40">Medicine brand name</span>
-                        <input required type="text" ref={brandNameRef} placeholder="Medicine brand name" className="border-4 border-indigo-500/100 ..." />
+                        <span className="w-40 font-bold">Medicine Brand</span>
+                        <input required type="text" ref={brandNameRef} placeholder="Enter Medicine Brand Name" className="border-4 border-indigo-500/100 ..." />
                     </label>
                     <label className="input-group">
-                        <span className="w-40">Medicine brand id</span>
-                        <input required type="text" ref={brandIdRef} placeholder="Medicine brand id" className="border-4 border-indigo-500/100 ..." />
+                        <span className="w-40 font-bold">Medicine Brand Id</span>
+                        <input required type="text" ref={brandIdRef} placeholder="Enter Medicine Brand Id" className="border-4 border-indigo-500/100 ..." />
                     </label>
-                    <div className='text-center'>
-                        <button> ADD Medicine</button>
+                    <div className='text-center m-5'>
+                        <button className='bg-[#0E7490]'> ADD Medicine</button>
                     </div>
                 </form>
             </div>
