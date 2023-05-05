276e4251589cb69ebd82a716e03cac4d29bf112b
TouhedurRahman <touhedur.cse@gmail.com>
2023-05-05 18:41:15 +0600

    add medicine brand  page updated on dashboard

--- src/Pages/Dashboard/Medicine/MedicineBrand/AddMedicineBrand.js
+++ src/Pages/Dashboard/Medicine/MedicineBrand/AddMedicineBrand.js
@@ -7,21 +7,21 @@ const AddMedicineBrand = () => {
     const descriptionRef = useRef();
     const brandTitleRef = useRef();
     const imgRef = useRef();
-    const [error,setError] = useState("")
+    const [error, setError] = useState("");
+
     const handleOnSubmit = (e) => {
         axios.post('http://localhost:5000/api/v1/medicinebrand', {
             name: nameRef.current.value,
             description: descriptionRef.current.value,
-            brandTitle:brandTitleRef.current.value,
-            imgRef : imgRef.current.value
+            brandTitle: brandTitleRef.current.value,
+            imgRef: imgRef.current.value
         })
             .then(res => {
-                toast.success(<h2>congrats you can successfully added brand  </h2>)
+                toast.success("Medicine Brand added successfully.")
             })
             .catch(error => {
-                // setError(error.response.data.error)
-                toast.error(<h1>  {error?.response?.data?.error}</h1>);
-                 
+                toast.error(`${error?.response?.data?.error}`);
+
             })
         nameRef.current.value = ""
         brandTitleRef.current.value = ""
@@ -29,35 +29,42 @@ const AddMedicineBrand = () => {
         imgRef.current.value = ""
         e.preventDefault()
     }
+
     return (
         <div className='container mx-auto'>
-            {error&& <h1>{error}</h1>}
-        <h1 className='text-center text-3xl text-indigo-800 my-10 '>Add Medicine Brand</h1>
-        <div className="form-control px-4">
-            <form className='bg-zinc-200 p-4' onSubmit={handleOnSubmit}>
-                <label className="input-group">
-                    <span className="w-40">Brand Name</span>
-                    <input required type="text" ref={nameRef} placeholder="Enter brand name"className="border-4 border-indigo-500/100 ..." />
-                </label>
-                <label className="input-group">
-                    <span className="w-40">Brand Image Url</span>
-                    <input required type="text" ref={imgRef} placeholder="Enter img link"  className="border-4 border-indigo-500/100 ..." />
-                </label>
-                <label className="input-group">
-                    <span className="w-40">Brand Sub Title</span>
-                    <input required type="text" ref={brandTitleRef} placeholder="Enter Specialist" className="border-4 border-indigo-500/100 ..." />
-                </label> 
-                <label className="input-group">
-                    <span className="w-40">brand Description</span>
-                    <input required type="text" ref={descriptionRef} placeholder="Enter qualification" className="border-4 border-indigo-500/100 ..." />
-                </label>
-              
-               <div className='text-center'>
-               <button> ADD MedicineBrand</button>
-               </div>
-            </form>
+            {error && <h1>{error}</h1>}
+            <div>
+                <h3 className='text-3xl text-center text-[#0E7490] font-bold m-5'>
+                    <i>
+                        Add Medicine Brand_
+                    </i>
+                </h3>
+            </div>
+            <div className="form-control px-4">
+                <form className='bg-zinc-200 p-4 rounded-xl' onSubmit={handleOnSubmit}>
+                    <label className="input-group">
+                        <span className="w-40 font-bold">Brand Name</span>
+                        <input required type="text" ref={nameRef} placeholder="Enter brand name" className="border-4 border-indigo-500/100 ..." />
+                    </label>
+                    <label className="input-group">
+                        <span className="w-40 font-bold">Brand Image</span>
+                        <input required type="text" ref={imgRef} placeholder="Enter Brand Image URL" className="border-4 border-indigo-500/100 ..." />
+                    </label>
+                    <label className="input-group">
+                        <span className="w-40 font-bold">Subtitle</span>
+                        <input required type="text" ref={brandTitleRef} placeholder="Enter Brand Subtitle" className="border-4 border-indigo-500/100 ..." />
+                    </label>
+                    <label className="input-group">
+                        <span className="w-40 font-bold">Brand Description</span>
+                        <input required type="text" ref={descriptionRef} placeholder="Enter Brand Description" className="border-4 border-indigo-500/100 ..." />
+                    </label>
+
+                    <div className='text-center m-5'>
+                        <button className='bg-[#0E7490]'> ADD MedicineBrand</button>
+                    </div>
+                </form>
+            </div>
         </div>
-    </div>
     );
 };
 
