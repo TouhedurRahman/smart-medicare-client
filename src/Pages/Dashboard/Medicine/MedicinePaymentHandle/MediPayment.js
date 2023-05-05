d8b1f2f2b2420b6153f29cfde5a8f9e33ffd37da
TouhedurRahman <touhedur.cse@gmail.com>
2023-05-05 16:12:34 +0600

    dashboard medicine order contoll page updated

--- src/Pages/Dashboard/Medicine/MedicinePaymentHandle/MediPayment.js
+++ src/Pages/Dashboard/Medicine/MedicinePaymentHandle/MediPayment.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { toast } from 'react-hot-toast';
+import { BsCheckCircleFill } from 'react-icons/bs';
 
 const MediPayment = () => {
     const [order, setOrder] = useState([]);
@@ -23,24 +24,68 @@ const MediPayment = () => {
 
     return (
         <div className='container mx-auto max-h-500 overflow-y-auto'>
-            <div className='grid grid-cols-1 lg:grid-cols-4 sm:grid-cols-3 gap-4'>
-                {
-                    order.map((item) => <div className="bg-white rounded-lg shadow-md p-4">
-                        <h2 className="text-lg font-medium text-gray-800">cus name : {item?.cus_name}</h2>
-                        <h2 className="text-lg font-medium text-gray-800">product : {item?.product_name}</h2>
+            <div div className="overflow-x-auto">
+                <div>
+                    <h3 className='text-3xl text-center text-[#0E7490] font-bold m-5'>
+                        <i>
+                            Medicine Ordered by Customers_
+                        </i>
+                    </h3>
+                </div>
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th className='text-center'>Sl. No.</th>
+                            <th>Customer Name</th>
+                            <th>Product</th>
+                            <th>Delivery Address</th>
+                            <th className='text-center'>Mobile No.</th>
+                            <th className='text-right'>Total Amount</th>
+                            <th className='text-center'>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            order.map((item, i) => <tr>
+                                <th className='text-center'>
+                                    {
+                                        `${i + 1}` < 10
+                                            ?
+                                            `0${i + 1}`
+                                            :
+                                            `${i + 1}`
+                                    }
+                                </th>
+                                <td>{item?.cus_name}</td>
+                                <td>{item?.product_name}</td>
+                                <td>{item?.cus_city}</td>
+                                <td className='text-center'>{item?.cus_phone}</td>
+                                <td className='text-right'>৳ {item?.total_amount}/-</td>
+                                <td className='text-center'>
 
-                        <p className="mt-2 text-gray-600">payment status : {item?.payment}</p>
-                        <p className="mt-2 text-gray-600">cus city : {item?.cus_city}</p>
-                        <p>deliver : {item?.status}</p>
-                        <button
-                            onClick={() => onSubmit(item?._id)}
-                        >
-                            {
-                                item.status === "deliver" ? "deliver" : "pending"
-                            }
-                        </button>
-                    </div>)
-                }
+                                    {
+                                        item.status === 'deliver'
+                                            ?
+                                            <p className='flex justify-center items-center italic text-[green] font-bold'>
+                                                <BsCheckCircleFill className='mr-1' /><>Delivered</>
+                                            </p>
+                                            :
+                                            <div className='flex justify-center items-center'>
+                                                <button
+                                                    className='w-50 bg-[#0E7490]'
+                                                    onClick={() => onSubmit(item?._id)}
+                                                >
+                                                    <p className='flex justify-center items-center text-[white] font-bold'>
+                                                        <BsCheckCircleFill className='mr-1' /><>Make Delivery</>
+                                                    </p>
+                                                </button>
+                                            </div>
+                                    }
+                                </td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
             </div>
         </div>
     );
