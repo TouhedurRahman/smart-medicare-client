44f77b5909f38a6b31e57f484dbdbb8b4572482c
TouhedurRahman <touhedur.cse@gmail.com>
2023-05-05 17:54:51 +0600

    add doctor category updated on dashboard page

--- src/Pages/Dashboard/DoctorHandle/DoctorCategory/AddDoctorCategory.js
+++ src/Pages/Dashboard/DoctorHandle/DoctorCategory/AddDoctorCategory.js
@@ -1,65 +1,69 @@
 import axios from 'axios';
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import { toast } from 'react-hot-toast';
 
 const AddDoctorCategory = () => {
-
     const imgRef = useRef();
     const descriptionRef = useRef();
     const categoryRef = useRef();
     const subtitleRef = useRef();
+
     const handleOnSubmit = (e) => {
         axios.post('http://localhost:5000/api/v1/addDoctorCategory', {
-
             img: imgRef.current.value,
             description: descriptionRef.current.value,
             category: categoryRef.current.value,
-            subtitle:subtitleRef.current.value     
-           })
+            subtitle: subtitleRef.current.value
+        })
             .then(res => {
-                toast.success(<h2>congrats you can successfully added doctor category</h2>)
+                toast.success("Doctor Added successfully.")
             })
             .catch(error => {
-                toast.error(<h1>  {error?.response?.data?.error}</h1>);
+                toast.error(`${error?.response?.data?.error}`);
             })
-        
         descriptionRef.current.value = ""
         categoryRef.current.value = ""
-        imgRef.current.value =""
+        imgRef.current.value = ""
         subtitleRef.current.value = ""
         e.preventDefault()
     }
+
     return (
         <div className='container mx-auto'>
-        <h1 className='text-center text-3xl text-indigo-800 my-10 '>Add  Doctor Category</h1>
-        <div className="form-control">
-            <form className='bg-zinc-200 p-3' onSubmit={handleOnSubmit}>
-               
-                <label className="input-group">
-                    <span className="w-40">Doctor Image </span>
-                    <input required type="text" ref={imgRef} placeholder="Enter img link" className="border-4 border-indigo-500/100 ..." />
-                </label>
-                <label className="input-group">
-                    <span className="w-40">category</span>
-                    <input required type="text"  ref={categoryRef} placeholder="Enter category" className="border-4 border-indigo-500/100 ..." />
-                </label>
-                <label className="input-group">
-                    <span className="w-40">subtitle</span>
-                    <input required type="text" ref={subtitleRef}  placeholder="Enter category" className="border-4 border-indigo-500/100 ..." />
-                </label>
-               
-                <label className="input-group">
-                    <span className="w-40">description</span>
-                    <input required type="text" ref={descriptionRef} placeholder="Enter description" className="border-4 border-indigo-500/100 ..." />
-                </label>
-                
-               <div className='text-center'>
-                 
-               <button className='text-center p-4'> ADD DOCTOR CATEGORY</button>
-               </div>
-            </form>
+            <div>
+                <h3 className='text-3xl text-center text-[#0E7490] font-bold m-5'>
+                    <i>
+                        Add Doctor Category_
+                    </i>
+                </h3>
+            </div>
+            <div className="form-control">
+                <form className='bg-zinc-200 p-3 rounded-xl' onSubmit={handleOnSubmit}>
+
+                    <label className="input-group">
+                        <span className="w-40 font-bold">Category Image </span>
+                        <input required type="text" ref={imgRef} placeholder="Enter Doctor Category Image URL" className="border-4 border-indigo-500/100 ..." />
+                    </label>
+                    <label className="input-group">
+                        <span className="w-40 font-bold">Category Name</span>
+                        <input required type="text" ref={categoryRef} placeholder="Enter Doctor Category Name" className="border-4 border-indigo-500/100 ..." />
+                    </label>
+                    <label className="input-group">
+                        <span className="w-40 font-bold">Subtitle</span>
+                        <input required type="text" ref={subtitleRef} placeholder="Enter Doctor Category Subtitle" className="border-4 border-indigo-500/100 ..." />
+                    </label>
+
+                    <label className="input-group">
+                        <span className="w-40 font-bold">Description</span>
+                        <input required type="text" ref={descriptionRef} placeholder="Enter Doctor Category Description" className="border-4 border-indigo-500/100 ..." />
+                    </label>
+
+                    <div className='text-center m-5'>
+                        <button className='text-center p-4 bg-[#0E7490]'> ADD DOCTOR CATEGORY</button>
+                    </div>
+                </form>
+            </div>
         </div>
-    </div>
     );
 };
 
