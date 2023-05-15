334d395fb424881de52b51362102016379bafa87
TouhedurRahman <touhedur.cse@gmail.com>
2023-05-15 23:20:57 +0600

    cart security added

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
       user?.email
       &&
       <li tabIndex={0}>
-        <Link className="justify-between ">
+        <Link className="justify-between">
           Order
           <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
         </Link>
@@ -86,27 +86,33 @@ const Navbar = () => {
             </ul>
           </div>
           <div className='navbar navbar-end'>
-            {/* start of card  */}
-            <div className="flex-none">
-              <div className="dropdown dropdown-end">
-                <label tabIndex={0} className="btn btn-ghost btn-circle">
-                  <div className="indicator">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-                    <span className="badge badge-sm indicator-item">{state?.cart?.length}</span>
-                  </div>
-                </label>
-                <div tabIndex={0} className="mt-3 card card-compact dropdown-content w-52 bg-[#0E7490] shadow">
-                  <div className="card-body text-center">
-                    <span className="font-bold text-lg">{state?.cart?.length} Items</span>
-                    {/* <span className="text-info">Subtotal: $999</span> */}
-                    <div className="text-center py-5">
-                      <Link to="/show/cart" className='text-white font-bold border border-[white] px-5 py-2 rounded-lg'>
-                        View cart
-                      </Link>
+            {/* start of cart  */}
+            <div>
+              {
+                user?.email
+                &&
+                <div className="flex-none mr-2">
+                  <div className="dropdown dropdown-end">
+                    <label tabIndex={0} className="btn btn-ghost btn-circle">
+                      <div className="indicator">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                        <span className="badge badge-sm indicator-item">{state?.cart?.length}</span>
+                      </div>
+                    </label>
+                    <div tabIndex={0} className="mt-3 card card-compact dropdown-content w-52 bg-[#0E7490] shadow">
+                      <div className="card-body text-center">
+                        <span className="font-bold text-lg">{state?.cart?.length} Items</span>
+                        {/* <span className="text-info">Subtotal: $999</span> */}
+                        <div className="text-center py-5">
+                          <Link to="/show/cart" className='text-white font-bold border border-[white] px-5 py-2 rounded-lg'>
+                            View cart
+                          </Link>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
+              }
             </div>
             {
               user?.email
