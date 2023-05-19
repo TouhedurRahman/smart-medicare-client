d2d448e1341f104ea011c67c76687f58ce235209
TouhedurRahman <touhedur.cse@gmail.com>
2023-05-19 11:11:32 +0600

    search box design updated in all page

--- src/Pages/Home/Ambulance/AmbulanceDetails/AmbulanceDetailsServices.js
+++ src/Pages/Home/Ambulance/AmbulanceDetails/AmbulanceDetailsServices.js
@@ -54,17 +54,32 @@ const AmbulanceDetailsServices = () => {
 
     return (
         <div className='container mx-auto'>
-
-            <label for="simple-search" class="sr-only">Search</label>
-            <div class="relative w-full">
-                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                    <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+            <label htmlFor="simple-search" className="sr-only">Search</label>
+            <div className="relative w-full">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <svg
+                        aria-hidden="true"
+                        className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                            clipRule="evenodd"
+                        ></path>
+                    </svg>
                 </div>
-                <input onChange={handlerLocation} type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search location" required />
+                <input
+                    onChange={handlerLocation}
+                    type="text" id="simple-search"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Search Location"
+                    required
+                />
             </div>
 
-
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-auto px-4 py-10   justify-center align-center">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-auto px-4 py-5   justify-center align-center">
                 {
                     ambulance.map((ambulanceData, index) => <div
                         className="card bg-base-100 shadow-xl"
--- src/Pages/Home/Doctors/DoctorService/FilterDoctor.js
+++ src/Pages/Home/Doctors/DoctorService/FilterDoctor.js
@@ -1,11 +1,34 @@
-import React  from 'react';
+import React from 'react';
 const FilterDoctor = (props) => {
     const setValue = e => {
         props.setSearchDoctor(e.target.value)
     }
     return (
-        <div style={{ margin: "12px 0px 25px 0px" }}>             
-                <input type="text"  onChange={(e) => setValue(e)} placeholder="Search necessery doctor" className="input input-bordered border-purple-900" />
+        <div style={{ margin: "12px 0px 25px 0px" }}>
+            <label htmlFor="simple-search" className="sr-only">Search</label>
+            <div className="relative w-full">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <svg
+                        aria-hidden="true"
+                        className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                            clipRule="evenodd"
+                        ></path>
+                    </svg>
+                </div>
+                <input
+                    onChange={(e) => setValue(e)}
+                    type="text" id="simple-search"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Search Necessary Doctor"
+                    required
+                />
+            </div>
         </div>
     );
 };
