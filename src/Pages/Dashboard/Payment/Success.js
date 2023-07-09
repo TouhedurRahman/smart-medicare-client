8ce008b8053cbed779897b6d4914209374fff184
TouhedurRahman <touhedur.cse@gmail.com>
2023-07-10 01:29:55 +0600

    payment success design updated

--- src/Pages/Dashboard/Payment/Success.js
+++ src/Pages/Dashboard/Payment/Success.js
@@ -1,48 +1,43 @@
 import axios from 'axios';
-import React, {  useEffect, useState } from 'react';
-import {    useNavigate, useParams } from 'react-router-dom';
-import img from '../../../assets/ui-animation.gif'
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import img from '../../../assets/ui-animation.gif';
+
 const Success = () => {
-    const {id} = useParams()
-    const [order,setOrder] = useState({})
-    const navigate = useNavigate()
-    useEffect(()=>{
+    const { id } = useParams();
+    const [order, setOrder] = useState({});
+    const navigate = useNavigate();
+
+    useEffect(() => {
         axios.get(`http://localhost:5000/api/v1/order/${id}`)
-        .then(response=>{
-           
-            setOrder(response?.data?.result)
-        })
-        .catch(error=>{
-            
-        })
-    },[id])
-    const paymentSuccess = e =>{
-         
-        axios.post(`http://localhost:5000/api/v1/order`,{
-            tran_id:id,
-            val_id:order?.val_id
-        })
-        .then(response=>{
-            
-            // setOrder(response?.data?.result)
-           navigate("/")
+            .then(response => {
+                setOrder(response?.data?.result)
+            })
+            .catch(error => { })
+    }, [id])
 
+    const paymentSuccess = e => {
+        axios.post(`http://localhost:5000/api/v1/order`, {
+            tran_id: id,
+            val_id: order?.val_id
         })
-        .catch(error=>{
-            
-        })
+            .then(response => {
+                navigate("/");
+            })
+            .catch(error => { })
     }
-    return (
-        <div className='container mx-auto text-center'>
-             {/* <img src={img} alt="...loading"  /> */}
-             <h1> name of customer  {order?.cus_name} </h1>
-             <button onClick={()=>paymentSuccess(id)}>confirm order</button> 
-             <h1>
-                 
-             </h1>
-             <h2>
 
-             </h2>
+    return (
+        <div>
+            <div className='mx-auto text-center rounded-full'>
+                <div className='m-5'>
+                    <img src={img} alt="...loading" style={{ display: 'block', margin: '0 auto', borderRadius: '10%' }} />
+                </div>
+            </div>
+            <div className='text-center'>
+                {/* <h1> name of customer  {order?.cus_name} </h1> */}
+                <button className='bg-[#0E7490]' onClick={() => paymentSuccess(id)}>Confirm Order</button>
+            </div>
         </div>
     );
 };
